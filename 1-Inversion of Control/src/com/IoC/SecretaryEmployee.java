e9c86fa02270eed8616793690dc2a0821ef09637
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-12-11 11:22:57 -0300

    Inyection dependencies with contructors and with setters.

--- 1-Inversion of Control/src/com/IoC/SecretaryEmployee.java
+++ 1-Inversion of Control/src/com/IoC/SecretaryEmployee.java
@@ -3,10 +3,6 @@ package com.IoC;
 public class SecretaryEmployee implements Employees {
 	private ReportsCreation newReport;
 	
-	public SecretaryEmployee(ReportsCreation newReport) {
-		this.newReport = newReport;
-	}
-	
 	@Override
 	public String getTasks() {
 		return "I manage the agenda of the bosses.";
@@ -16,6 +12,14 @@ public class SecretaryEmployee implements Employees {
 	public String getReport() {
 		// TODO Auto-generated method stub
 		return "Report created by the Secretary: " + newReport.getReport();
+	}
+
+	public ReportsCreation getNewReport() {
+		return newReport;
+	}
+
+	public void setNewReport(ReportsCreation newReport) {
+		this.newReport = newReport;
 	}
 
 }
